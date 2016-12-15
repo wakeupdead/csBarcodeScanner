6dc6e3a029df983d1314ef0e22b3816511ff8ca0
wakeupdead <wakeupdead@inbox.ru>
2016-12-15 16:29:58 +0300

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+
+import { NavController, AlertController } from 'ionic-angular';
+
+import { BarcodeScanner } from 'ionic-native';
+import { Geolocation } from 'ionic-native';
+
+import {ItemModel} from 'model';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  barcodes:any = [];
+  id:number = 0;
+
+  positionWatch:any;
+  lat:any;
+  lon:any;
+  timestamp:any;
+
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+    this.positionWatch = Geolocation.watchPosition();
+    this.positionWatch.subscribe((data) => {
+    // data can be a set of coordinates, or an error (if an error occurred).
+      this.lat = data.coords.latitude;
+      this.lon = data.coords.longitude;
+      this.timestamp = data.timestamp;
+    });
+    
+  }
+
+  scan():void {
+    BarcodeScanner.scan().then((barcodeData) => {
+      // Success! Barcode data is here
+      if (!barcodeData.cancelled) {
+        this.id = this.id++;
+        this.barcodes.push({
+            id: this.id,
+            text: barcodeData.text,
+            lat: this.lat,
+            lon: this.lon
+          })
+      } else {
+        this.presentAlert('Scan cancelled');
+      }
+      
+    }, (err) => {
+        // An error occurred
+        this.presentAlert('Error scanning barcode');
+    });
+  }
+
+  presentAlert(text:string) {
+  let alert = this.alertCtrl.create({
+    title: 'Alert',
+    subTitle: text,
+    buttons: ['Ok']
+  });
+  alert.present();
+}
+
+}
